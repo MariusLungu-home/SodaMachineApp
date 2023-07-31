2307ebb4cfafd3a7c3b8c61263f405a8e39067c1
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-07-31 09:27:00 +0200

    nothing new

--- SodaMachineLibrary.Tests/SodaMachineLogicTests.cs
+++ SodaMachineLibrary.Tests/SodaMachineLogicTests.cs
@@ -4,64 +4,73 @@ using SodaMachineLibrary.Logic;
 using SodaMachineLibrary.Models;
 using SodaMachineLibrary.Tests.Mocks;
 
-namespace SodaMachineLibrary.Tests
+namespace SodaMachineLibrary.Tests;
+
+public class SodaMachineLogicTests
 {
-    public class SodaMachineLogicTests
+    [Fact]
+    public void AddCoinToInventory_ShouldWork()
+    { 
+        MockDataAccess dataAccess = new();
+        SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
+
+        List<CoinModel> coins = new List<CoinModel>
+        {
+            new CoinModel { Name = "Quarter", Value = 0.25M },
+            new CoinModel { Name = "Dime", Value = 0.10M },
+            new CoinModel { Name = "Nickel", Value = 0.05M },
+            new CoinModel { Name = "Penny", Value = 0.01M }
+        };
+
+        logic.AddCoinToInventory(coins);
+
+        int expected = 5;
+        int actual = dataAccess.CoinInventory.Where(x => x.Name == "Quarter").Count();
+        
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void AddToSodaInventory_ShouldWork()
     {
-        [Fact]
-        public void AddCoinToInventory_ShouldWork()
-        { 
-            MockDataAccess dataAccess = new();
-            SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
-
-            List<CoinModel> coins = new List<CoinModel>
-            {
-                new CoinModel { Name = "Quarter", Value = 0.25M },
-                new CoinModel { Name = "Dime", Value = 0.10M },
-                new CoinModel { Name = "Nickel", Value = 0.05M },
-                new CoinModel { Name = "Penny", Value = 0.01M }
-            };
-
-            logic.AddCoinToInventory(coins);
-
-            int expected = 5;
-            int actual = dataAccess.CoinInventory.Where(x => x.Name == "Quarter").Count();
-            
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void AddToSodaInventory_ShouldWork()
+        MockDataAccess dataAccess = new();
+        SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
+        List<SodaModel> sodas = new List<SodaModel>
         {
-            MockDataAccess dataAccess = new();
-            SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
-            List<SodaModel> sodas = new List<SodaModel>
-            {
-                new SodaModel { Name = "Cola", Price = 1.00M },
-                new SodaModel { Name = "Cola", Price = 1.00M }
-            };
+            new SodaModel { Name = "Cola", Price = 1.00M },
+            new SodaModel { Name = "Cola", Price = 1.00M }
+        };
 
-            logic.AddToInventory(sodas);
-            int expected = 7;
-            int actual = dataAccess.SodaInventory.Count();
-            Assert.Equal(expected, actual);
-        }
+        logic.AddToInventory(sodas);
+        int expected = 7;
+        int actual = dataAccess.SodaInventory.Count();
+        Assert.Equal(expected, actual);
+    }
 
-        [Fact]
-        public void EmptyMoneyFromMachine_ShouldWork() 
-        {
-            MockDataAccess dataAccess = new();
-            SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
+    [Fact]
+    public void EmptyMoneyFromMachine_ShouldWork() 
+    {
+        MockDataAccess dataAccess = new();
+        SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
 
-            var (_, expected, _) = dataAccess.MachineInfo;
-            decimal actual = logic.EmptyMoneyFromMachine();
+        var (_, expected, _) = dataAccess.MachineInfo;
+        decimal actual = logic.EmptyMoneyFromMachine();
 
-            Assert.Equal(expected, actual);
-            
-            expected = 0;
-            (_, actual, _) = dataAccess.MachineInfo;
+        Assert.Equal(expected, actual);
+        
+        expected = 0;
+        (_, actual, _) = dataAccess.MachineInfo;
 
-            Assert.Equal(expected, actual);
-        }
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GetCoinInventory_ShouldWork()
+    {
+        MockDataAccess dataAccess = new();
+        SodaMachineLogic logic = new SodaMachineLogic(dataAccess);
+        List<CoinModel> expected = dataAccess.CoinInventory;
+        List<CoinModel> actual = logic.GetCoinInventory();
+        Assert.Equal(expected, actual);
     }
 }
--- SodaMachineLibrary/DataAccess/DataAccess.cs
+++ SodaMachineLibrary/DataAccess/DataAccess.cs
@@ -1,82 +1,81 @@
 ﻿using SodaMachineLibrary.Models;
 
-namespace SodaMachineLibrary.DataAccess
+namespace SodaMachineLibrary.DataAccess;
+
+public class DataAccess : IDataAccess
 {
-    public class DataAccess : IDataAccess
+    public void CoinInventory_AddCoin(List<CoinModel> coins)
     {
-        public void CoinInventory_AddCoin(List<CoinModel> coins)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public List<CoinModel> CoinInventory_GetAll()
-        {
-            throw new NotImplementedException();
-        }
+    public List<CoinModel> CoinInventory_GetAll()
+    {
+        throw new NotImplementedException();
+    }
 
-        public List<CoinModel> CoinInventory_WithdrawCoins(decimal coinValue, int quantity)
-        {
-            throw new NotImplementedException();
-        }
+    public List<CoinModel> CoinInventory_WithdrawCoins(decimal coinValue, int quantity)
+    {
+        throw new NotImplementedException();
+    }
 
-        public decimal MachineInfo_CashOnHand()
-        {
-            throw new NotImplementedException();
-        }
+    public decimal MachineInfo_CashOnHand()
+    {
+        throw new NotImplementedException();
+    }
 
-        public decimal MachineInfo_EmptyCash()
-        {
-            throw new NotImplementedException();
-        }
+    public decimal MachineInfo_EmptyCash()
+    {
+        throw new NotImplementedException();
+    }
 
-        public decimal MachineInfo_SodaPrice()
-        {
-            throw new NotImplementedException();
-        }
+    public decimal MachineInfo_SodaPrice()
+    {
+        throw new NotImplementedException();
+    }
 
-        public decimal MachineInfo_TotalIncome()
-        {
-            throw new NotImplementedException();
-        }
+    public decimal MachineInfo_TotalIncome()
+    {
+        throw new NotImplementedException();
+    }
 
-        public void SodaInventory_AddSodas(List<SodaModel> sodaModel)
-        {
-            throw new NotImplementedException();
-        }
+    public void SodaInventory_AddSodas(List<SodaModel> sodaModel)
+    {
+        throw new NotImplementedException();
+    }
 
-        public List<SodaModel> SodaInventory_GetAll()
-        {
-            throw new NotImplementedException();
-        }
+    public List<SodaModel> SodaInventory_GetAll()
+    {
+        throw new NotImplementedException();
+    }
 
-        public SodaModel SodaInventory_GetSoda(SodaModel sodaModel)
-        {
-            throw new NotImplementedException();
-        }
+    public SodaModel SodaInventory_GetSoda(SodaModel sodaModel)
+    {
+        throw new NotImplementedException();
+    }
 
-        public List<SodaModel> SodaInventory_GetTypes()
-        {
-            throw new NotImplementedException();
-        }
+    public List<SodaModel> SodaInventory_GetTypes()
+    {
+        throw new NotImplementedException();
+    }
 
-        public void UserCredit_Clear(string userId)
-        {
-            throw new NotImplementedException();
-        }
+    public void UserCredit_Clear(string userId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public void UserCredit_Deposit(string userId)
-        {
-            throw new NotImplementedException();
-        }
+    public void UserCredit_Deposit(string userId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public decimal UserCredit_GetTotal(string userId)
-        {
-            throw new NotImplementedException();
-        }
+    public decimal UserCredit_GetTotal(string userId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public void UserCredit_Insert(string userId, decimal amount)
-        {
-            throw new NotImplementedException();
-        }
+    public void UserCredit_Insert(string userId, decimal amount)
+    {
+        throw new NotImplementedException();
     }
 }
