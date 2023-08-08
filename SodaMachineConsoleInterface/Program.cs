9876fd8c439d6abd43c207b692b05102c5360248
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-08-08 17:47:17 +0200

    added console app

---
+++ SodaMachineConsoleInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using SodaMachineLibrary;
+
+
+namespace SodaMachineConsoleInterface
+{
+    internal class Program
+    {
+        private IConfiguration _config;
+        public Program(IConfiguration config)
+        {
+            _config = config;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
