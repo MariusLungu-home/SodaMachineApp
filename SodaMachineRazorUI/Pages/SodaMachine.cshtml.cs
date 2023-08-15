118bcfb2b690f2d6ec52bf042de0a6495c693bed
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-08-15 18:27:21 +0200

    finished Razor pages UI

--- SodaMachineRazorUI/Pages/SodaMachine.cshtml.cs
+++ SodaMachineRazorUI/Pages/SodaMachine.cshtml.cs
@@ -8,17 +8,17 @@ namespace SodaMachineRazorUI.Pages
 {
     public class SodaMachineModel : PageModel
     {
-        private ISodaMachineLogic _sodaMachinelogic;
+        private readonly ISodaMachineLogic _sodaMachine;
 
-        public decimal SodaPrice { get;set; }
+        public decimal SodaPrice { get; set; }
         public decimal DepositedAmount { get; set; }
-        public List<SodaModel> SodaOptions{ get; set; }
+        public List<SodaModel> SodaOptions { get; set; }
 
         [BindProperty]
         public decimal Deposit { get; set; }
 
         [BindProperty]
-        public SodaModel SelectedSoda{ get; set; }
+        public SodaModel SelectedSoda { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string UserId { get; set; }
@@ -29,9 +29,9 @@ namespace SodaMachineRazorUI.Pages
         [BindProperty(SupportsGet = true)]
         public string ErrorMessage { get; set; }
 
-        public SodaMachineModel(ISodaMachineLogic sodaMachineLogic)
+        public SodaMachineModel(ISodaMachineLogic sodaMachine)
         {
-            _sodaMachinelogic = sodaMachineLogic;
+            _sodaMachine = sodaMachine;
         }
 
         public void OnGet()
@@ -41,56 +41,58 @@ namespace SodaMachineRazorUI.Pages
                 UserId = Guid.NewGuid().ToString();
             }
 
-            SodaPrice = _sodaMachinelogic.GetSodaPrice();
-            DepositedAmount = _sodaMachinelogic.GetMoneyInsertedTotal(UserId);
-            SodaOptions = _sodaMachinelogic.ListTypesOfSoda();
+            SodaPrice = _sodaMachine.GetSodaPrice();
+            DepositedAmount = _sodaMachine.GetMoneyInsertedTotal(UserId);
+            SodaOptions = _sodaMachine.ListTypesOfSoda();
         }
 
-        //used for depositing money
-        public IActionResult OnPost() 
+        // Used for depositing coins
+        public IActionResult OnPost()
         {
             if (Deposit > 0)
             {
-                _sodaMachinelogic.MoneyInserted(UserId, Deposit);
+                _sodaMachine.MoneyInserted(UserId, Deposit);
             }
 
             return RedirectToPage(new { UserId });
         }
 
-        //used for requesting soda
-        public IActionResult OnPostSoda() 
+        // Used for requesting soda
+        public IActionResult OnPostSoda()
         {
-            var results = _sodaMachinelogic.RequestSoda(SelectedSoda, UserId);
-            OutputText = string.Empty;
+            var results = _sodaMachine.RequestSoda(SelectedSoda, UserId);
+            OutputText = "";
 
+            // Handle the response
             if (results.errorMessage.Length > 0)
             {
                 ErrorMessage = results.errorMessage;
             }
-            else 
+            else
             {
-                OutputText = $"Enjoy your {results.soda.Name}!";
+                OutputText = $"Here is your {results.soda.Name}";
 
                 if (results.change.Count > 0)
                 {
-                    OutputText += $"<br><br>Don't forget your change:<br>";
+                    OutputText += "<br><br>Here is your change:<br>";
                     results.change.ForEach(x => OutputText += $"{x.Name}<br>");
                 }
-                else 
+                else
                 {
-                    OutputText += "<br><br>There is no change to refund.";
+                    OutputText += "<br><br>You used exact change so there is no change to refund.";
                 }
             }
+
             return RedirectToPage(new { UserId, ErrorMessage, OutputText });
         }
 
-        //used for refunding money
+        // Used for cancelling our deposit
         public IActionResult OnPostCancel()
         {
-            DepositedAmount = _sodaMachinelogic.GetMoneyInsertedTotal(UserId);
-            _sodaMachinelogic.IssueFullRefund(UserId);
+            DepositedAmount = _sodaMachine.GetMoneyInsertedTotal(UserId);
+            _sodaMachine.IssueFullRefund(UserId);
 
-            OutputText = $"You have been refunded {DepositedAmount}$.";
+            OutputText = $"You have been refunded {String.Format("{0:C}", DepositedAmount)}";
 
             return RedirectToPage(new { UserId, OutputText });
         }
